389d2e332a56d0bbed85bc37b4314b795f2a6ee5
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 14:20:46 +0400

    constructor is generated

--- trainee-app/src/main/java/com/example/trainee_app/InventoryBook.java
+++ trainee-app/src/main/java/com/example/trainee_app/InventoryBook.java
@@ -6,4 +6,10 @@ public class InventoryBook {
     double price;
     int stockCount;
 
+    public InventoryBook(int bookId, String title, double price, int stockCount) {
+        this.bookId = bookId;
+        this.title = title;
+        this.price = price;
+        this.stockCount = stockCount;
+    }
 }
