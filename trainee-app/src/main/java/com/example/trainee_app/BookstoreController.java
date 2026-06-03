ec15eb7af51a01a5cea12517c01ec87ae67e10aa
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 14:32:03 +0400

    addingBookToList() method is created

--- trainee-app/src/main/java/com/example/trainee_app/BookstoreController.java
+++ trainee-app/src/main/java/com/example/trainee_app/BookstoreController.java
@@ -1,5 +1,7 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,4 +10,10 @@ import java.util.List;
 @RestController
 public class BookstoreController {
     static List<InventoryBook> inventoryBookList = new ArrayList<>();
+
+    @GetMapping("/addingBookToList")
+    public String addingBookToList(@RequestParam int id, String title, double price, int stockCount){
+        inventoryBookList.add(new InventoryBook(id,title,price,stockCount));
+        return " Book is successfully added to the bookstore's catalog";
+    }
 }
