99f7d3c3932f5b86db7c2ff4bbda4dbedd6aa743
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 11:27:01 +0400

    constructor is generated

--- trainee-app/src/main/java/com/example/trainee_app/Author.java
+++ trainee-app/src/main/java/com/example/trainee_app/Author.java
@@ -5,4 +5,9 @@ public class Author {
     String name;
     String biography;
 
+    public Author(int id, String name, String biography) {
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
+    }
 }
