0e1ffa310d4a2496ba1b7360db9abbe14b8b56c2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 11:41:46 +0400

    addAuthor method is created

--- trainee-app/src/main/java/com/example/trainee_app/LibraryController.java
+++ trainee-app/src/main/java/com/example/trainee_app/LibraryController.java
@@ -1,5 +1,6 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,5 +10,11 @@ import java.util.List;
 public class LibraryController {
     private static List<Author> authorList = new ArrayList<>();
     private static List<Book> bookList =new ArrayList<>();
+
+    @GetMapping("/addAuthor")
+    public String addAuthor(int id, String name, String biography){
+        authorList.add(new Author(id,name,biography));
+        return "Author added successfully";
+    }
 
 }
