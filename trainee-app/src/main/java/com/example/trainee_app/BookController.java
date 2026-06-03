df48d567db95202a5a0cdb1abe0643fbf4e93fd8
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 09:41:07 +0400

    method add() is created

--- trainee-app/src/main/java/com/example/trainee_app/BookController.java
+++ trainee-app/src/main/java/com/example/trainee_app/BookController.java
@@ -1,12 +1,19 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 public class BookController {
     static private ArrayList<Book> books = new ArrayList<>();
 
+    @GetMapping("/add-book")
+    public String add(int id, String name){
+     books.add(new Book(id,name));
+     return  "Book added successfully!";
+    }
 
 }
