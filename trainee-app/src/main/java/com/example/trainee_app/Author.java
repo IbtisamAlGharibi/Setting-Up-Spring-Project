b8cfba6893139647e6992d974e56a530704a4db6
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 11:27:31 +0400

    getter and setter is generated

--- trainee-app/src/main/java/com/example/trainee_app/Author.java
+++ trainee-app/src/main/java/com/example/trainee_app/Author.java
@@ -10,4 +10,28 @@ public class Author {
         this.name = name;
         this.biography = biography;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
 }
