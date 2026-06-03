af3b6890ea34ab04d23c709dc60959b7ecfae985
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-03 09:30:01 +0400

    Book controller class is created

---
+++ trainee-app/src/main/java/com/example/trainee_app/BookController.java
@@ -0,0 +1,12 @@
+package com.example.trainee_app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class BookController {
+    static private ArrayList<Book> books = new ArrayList<>();
+
+
+}
